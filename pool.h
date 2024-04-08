fd2d44ca2021951d293f7986eebc9ab442c427fa
Earnest Republic <earnestr@student.21-school.ru>
2024-04-08 22:20:41 +0300

    Eh

--- pool.h
+++ pool.h
@@ -3,23 +3,46 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_set>
 
 class Pool {
     private:
         double litres;
+        std::unordered_set<unsigned long long> *id_connect;
     public:
-        Pool(double litr = 0) : litres(litr) {}
+        Pool(double litr = 0) : litres(litr), id_connect(nullptr) {}
         void AddLitres(double litr) { litres += litr; }
         void SetLitres(double litr) { litres = litr; }
         double GetLitres() const { return litres; }
+
+        void AddIdConnect(unsigned long long id) { 
+            if (id_connect == nullptr) {
+                id_connect = new std::unordered_set<unsigned long long>;
+            }
+            (*id_connect).insert(id);  
+        }
+
+        void RemoveIdConnect(unsigned long long id) {
+            if (id_connect != nullptr) {
+                (*id_connect).erase(id);
+            }
+            if ((*id_connect).size() == 0) {
+                delete id_connect;
+                id_connect = nullptr;
+            }
+        }
+
+        ~Pool() {
+            if (id_connect != nullptr) {
+                delete id_connect;
+            }
+        }
 };
 
 class GraphPool {
     private:
         std::vector<Pool> pools;  // вектор хранения объектов "Бассейн"
         unsigned long long size;  // количество бассейнов в графе
-        std::vector<unsigned long long> graph_connect_ind;    // словарь смежности бассейнов
-        std::vector<std::set<unsigned long long>> graph_connect_edge;
 
         unsigned long long max_litres;  // максимальный объем воды для добавленияль
         unsigned long long amount_edges;  // количество связей в графе
